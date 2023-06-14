cf65d32ea20eecaba1bcf8e65b3387dba8e95c8f
Massimo Colella <massimo-colella@outlook.it>
2023-06-14 17:29:46 +0200

    Creato sito statico

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,181 @@
+<footer>   
+    <div class="container">
+        <div class="footer-list">
+            <ul>
+                <h5>dc comics</h5>
+                <li>
+                    <a href="">
+                        <span>characters</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span>characters</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span>characters</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span>characters</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span>characters</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span>characters</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span>characters</span>
+                    </a>
+                </li>
+            </ul>
+
+            <ul>
+                <h5>dc</h5>
+                <li>
+                    <a href="">
+                        <span>terms of use</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span>terms of use</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span>terms of use</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span>terms of use</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span>terms of use</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span>terms of use</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span>terms of use</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span>terms of use</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span>terms of use</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span>terms of use</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span>terms of use</span>
+                    </a>
+                </li>
+            </ul>
+
+            <ul>
+                <h5>sites</h5>
+                <li>
+                    <a href="">
+                        <span>dc</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span>dc</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span>dc</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span>dc</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span>dc</span>
+                    </a>
+                </li>
+            </ul>
+
+            <ul>
+                <h5>shop</h5>
+                <li>
+                    <a href="">
+                        <span>shop dc</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span>shop dc</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</footer>
+<div id="footer-end">
+    <div class="container">
+        <button class="btn btn-dark">sign up now!</button>
+        <div class="footer-icons">
+            <h5>follow us</h5>
+            <ul>
+                <li>
+                    <a href="">
+                        <img src="{{Vite::asset('resources/img/footer-facebook.png')}}" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img src={{Vite::asset('resources/img/footer-twitter.png')}}" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img src={{Vite::asset('resources/img/footer-youtube.png')}}" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="">       
+                        <img src="{{Vite::asset('resources/img/footer-pinterest.png')}}" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img src="{{Vite::asset('resources/img/footer-periscope.png')}}" alt="">
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+<header>
+    <div class="container">
+        <div>
+            <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="Logo">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="">characters</a></li>
+                <li><a href="">comics</a></li>
+                <li><a href="">movies</a></li>
+                <li><a href="">TV</a></li>
+                <li><a href="">games</a></li>
+                <li><a href="">collectibles</a></li>
+                <li><a href="">videos</a></li>
+                <li><a href="">fans</a></li>
+                <li><a href="">news</a></li>
+                <li><a href="">shop</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
