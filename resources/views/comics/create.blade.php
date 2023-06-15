50584e1b9e9092d5b6e14e804463281a82c41b52
Massimo Colella <massimo-colella@outlook.it>
2023-06-15 15:52:10 +0200

    Aggiustato bug della create e dello store

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -11,11 +11,27 @@
         </div>
         <div class="mb-3">
             <label for="text" class="form-label">Descrizione</label>
-            <input type="text" class="form-control" id="description" aria-describedby="emailHelp" name="description">
+            <input type="textarea" class="form-control" id="description" aria-describedby="emailHelp" name="description">
         </div>
         <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">prezzo</label>
-            <input type="text" class="form-control" id="price" name="price">
+            <label for="exampleInputPassword1" class="form-label">thumb</label>
+            <input type="textarea" class="form-control" id="thumb" name="thumb">
+        </div>
+        <div class="mb-3">
+            <label for="name" class="form-label">price</label>
+            <input type="text" class="form-control" id="price" aria-describedby="emailHelp" name="price">
+        </div>
+        <div class="mb-3">
+            <label for="name" class="form-label">series</label>
+            <input type="text" class="form-control" id="series" aria-describedby="emailHelp" name="series">
+        </div>
+        <div class="mb-3">
+            <label for="name" class="form-label">sale_date</label>
+            <input type="text" class="form-control" id="sale_date" aria-describedby="emailHelp" name="sale_date">
+        </div>
+        <div class="mb-3">
+            <label for="name" class="form-label">type</label>
+            <input type="text" class="form-control" id="type" aria-describedby="emailHelp" name="type">
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
